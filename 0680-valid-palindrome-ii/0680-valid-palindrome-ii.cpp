23eeaaf2cf60750368c0be087589658cf9167e41
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-05-21 22:03:41 +0530

    Time: 53 ms (58.72%), Space: 24 MB (41.90%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+bool checkPalindrome(string s, int i, int j){
+    while(i<=j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+        else{
+            i++;
+            j--;
+        }
+       
+    }
+     return true;
+}
+
+
+    bool validPalindrome(string s) {
+        int n = s.length();
+        int i =0;
+        int j = n-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                //ek bar i ko remove ek bar j ko remove 
+                return checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1);
+
+            }else{
+               i++;
+               j--;
+
+            }
+        }
+        return true;
+        
+    }
+
+
+};
