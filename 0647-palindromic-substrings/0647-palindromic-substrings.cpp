57d5ae8f193ab8163f0e983cc84620bf4121795f
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-05-28 21:47:05 +0530

    Time: 7 ms (70.55%), Space: 13.1 MB (45.19%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int expandAround(string s,int i,int j){
+    int count =0;
+    while(i>=0 && j<s.length() && s[i]==s[j]){
+        count++;
+        i--;
+        j++;
+
+    }
+    return count;
+
+}
+    int countSubstrings(string s) {
+       int count =0;
+       int n = s.length();
+       for(int i =0;i<n;i++){
+           //odd case
+           int oddCase = expandAround(s,i,i);
+           count = count + oddCase;
+
+           int evenCase = expandAround(s,i,i+1);
+           count = count + evenCase;
+
+           //even case
+       } 
+       return count;
+    }
+};
