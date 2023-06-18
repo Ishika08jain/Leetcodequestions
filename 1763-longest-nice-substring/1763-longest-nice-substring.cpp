e4199206cac75bcd6bb8fade32f8614bf62c8e5b
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-06-18 23:34:55 +0530

    Time: 8 ms (79.63%), Space: 12 MB (69.85%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        return fun(s);
+        
+    }
+    string fun(string str){
+        if(str.length() <=1){
+            return "";
+        }
+        unordered_set <char> st;
+        st.insert(str.begin() , str.end());
+        for(int i =0;i< str.length();i++){
+            char ch = str[i];
+            char temp1 = toupper(ch);
+            char temp2 = tolower(ch);
+            if(st.find(temp1) == st.end() || st.find(temp2) == st.end()){
+                string left = fun(str.substr(0,i));
+                string right = fun(str.substr(i+1,str.length()));
+                if(left.length() ==  right.length()){
+                    return left;
+                    
+                }
+                else if(left.length() < right.length()){
+                    return right;
+                }
+                else {
+                    return left;
+                }
+            }
+        }
+    return str;
+}
+    
+};
