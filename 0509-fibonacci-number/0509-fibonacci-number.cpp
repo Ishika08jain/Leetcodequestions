4111761f9bd3c5ce3e39fc6737fe5b514f082cba
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-07-04 10:28:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    int recSolve(int n){
+        if(n==1 || n==0){
+            return n;
+        }
+        int ans = recSolve(n-1) + recSolve(n-2);
+        return ans;
+        
+    }
+    int topDown(vector <int> &dp, int n){
+         if(n==1 || n==0){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n] = topDown(dp, n-1) + topDown(dp,n-2);
+        return dp[n];
+        
+    
+        
+    }
     int fib(int n) {
-        
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1) + fib(n-2);
-        
-       
+       vector <int> dp(n+1,-1);
+        int ans = topDown(dp, n);
+        return ans;
+    
         
         
     }
