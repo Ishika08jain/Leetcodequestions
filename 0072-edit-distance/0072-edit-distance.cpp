ada4b453e6fa805a4e5342a48c39ff30237087aa
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-07-11 23:27:42 +0530

    Time: 79 ms (16.24%), Space: 81.3 MB (11.64%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,64 @@
 class Solution {
 public:
+//     int solveRec(string a, string b, int i , int j){
+//         if(i==a.length()){
+//             return b.length()-j;
+//         }
+//         if(j == b.length()){
+//             return a.length()-i;
+            
+//         }
+//         int ans =0;
+//         if(a[i]==b[j]){
+//             ans = solveRec(a,b,i+1,j+1);
+            
+//         }
+//         else{
+//             int insert = 1 + solveRec(a,b,i,j+1);
+//             int deleted = 1+ solveRec(a, b, i+1, j);
+//             int replace = 1+ solveRec(a,b,i+1,j+1);
+//             ans = min(insert,min( deleted, replace));
+//         }
+//         return ans;
+//     }
+     int solveMem(string a, string b, int i , int j, vector <vector <int>>& dp){
+        if(i==a.length()){
+            return b.length()-j;
+        }
+        if(j == b.length()){
+            return a.length()-i;
+            
+        }
+         if(dp[i][j]!= -1){
+             return dp[i][j];
+         }
+        int ans =0;
+        if(a[i]==b[j]){
+            ans = solveMem(a,b,i+1,j+1, dp);
+            
+        }
+        else{
+            int insert = 1 + solveMem(a,b,i,j+1, dp);
+            int deleted = 1+ solveMem(a, b, i+1, j, dp);
+            int replace = 1+ solveMem(a,b,i+1,j+1, dp);
+            ans = min(insert,min( deleted, replace));
+            
+        }
+         dp[i][j]= ans;
+        return dp[i][j];
+    }
     int minDistance(string word1, string word2) {
-        int a = word1.length();
-        int b = word2.length();
-
-        vector<vector<int>> dp(a+1, vector<int> (b+1));
-        for(int i=0;i<=a;i++){
-            dp[i][0]= i;
+        if(word1.length() == 0){
+            return word2.length();
         }
-        for(int j =0;j<=b;j++){
-            dp[0][j]= j;
+        if(word2.length()== 0){
+            return word1.length();
+            
         }
-        for(int i =1;i<=a;i++){
-            for(int j =1;j<=b;j++){
-                if(word1[i-1]== word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-
-
-                }else{
-                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
-                }
-            }
-        }
-        return dp[a][b];
+        int i =0;
+        int j =0;
+        vector <vector <int>> dp(word1.length(), vector <int> (word2.length(), -1));
+        return solveMem(word1, word2, i, j, dp);
+        
     }
 };
