92cf2c3cca2eeae10f22aff685767973b0d9f35e
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-07-25 12:44:40 +0530

    Time: 17 ms (60.08%), Space: 10.8 MB (31.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    static bool cmp (pair <int, int> a, pair <int, int> b){
+        return a.first < b.first;
+    }
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector <int> ans;
-       unordered_map <int,int> mpp;
-       for(int i=0;i<nums.size();i++){
-           if(mpp.find(target- nums[i])!= mpp.end()){
-               ans.push_back(mpp[target- nums[i]]);
-               ans.push_back(i);
-               return ans;
-           }
-           mpp[nums[i]] =i;
-       }
-       return ans;
+      vector <pair <int, int>> data;
+        vector <int> ans;
+    for(int i =0;i<nums.size();i++){
+        int val = nums[i];
+        int index = i;
+        data.push_back({val, index});
         
+        
+    }
+    sort(data.begin(), data.end(), cmp);
+    int start = 0;
+    int end = data.size()-1;
+     while(start < end){
+         if(data[start].first + data[end].first == target){
+             ans.push_back(data[start].second);
+             ans.push_back(data[end].second);
+             break;
+         }
+         else if(data[start].first + data[end].first > target){
+             end--;
+         }
+         else{
+             start++;
+         }
+         }
+        
+     
+        return ans;
        
     
       
