b2b5c01f26b2a69c12a422da9e9b13ab5f6ed6bd
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-07-28 20:56:32 +0530

    Time: 3 ms (70.47%), Space: 7.4 MB (75.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int n;
+    long long dp[22][22][2];
     int solve(vector <int> &nums ,int i, int j, int chance){
         if(i>j){
             return 0;
         }
+        if(dp[i][j][chance] != -1){
+            return dp[i][j][chance];
+        }
         if(chance ==0){
-            return max(nums[i] + solve(nums, i+1,j,1) , nums[j] + solve(nums, i,j-1, 1));
+            return dp[i][j][chance]= max(nums[i] + solve(nums, i+1,j,1) , nums[j] + solve(nums, i,j-1, 1));
         }
         else{
-            return min(solve(nums, i+1,j,0), solve(nums,i,j-1,0));
+            return dp[i][j][chance] = min(solve(nums, i+1,j,0), solve(nums,i,j-1,0));
         }
     }
     bool PredictTheWinner(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
         long long t =0;
         n = nums.size();
         for(int i =0;i<n;i++){
