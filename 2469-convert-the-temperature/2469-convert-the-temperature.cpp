97dd472d67a5d7cdcbb6fd4283640151f8b3376a
Ishika Jain <80661675+Ishika08jain@users.noreply.github.com>
2023-07-31 14:25:08 +0530

    Time: 2 ms (36.90%), Space: 6.1 MB (44.11%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
         vector <double> ans;
-        double kel = celsius + 273.15;
-        double far = celsius * 1.80 + 32.00;
-        ans.push_back(kel);
-        ans.push_back(far);
+        
+        ans.push_back(celsius + 273.15);
+        ans.push_back(celsius * 1.80 + 32.00);
         return ans;
     }
 };
